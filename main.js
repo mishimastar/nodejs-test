5e5fa4a81dc4ba5d04a25e89fa556d4b0495e4f5
mishimastar <mishimastar3000@gmail.com>
2022-05-25 15:35:50 +0300

    Described classes for queues

--- main.js
+++ main.js
@@ -1,63 +1,102 @@
 const http = require('node:http');
 const EventEmitter = require('node:events')
 
-class Emitter extends EventEmitter {}
-
-const myEmitter = new Emitter();
+const myEmitter = new EventEmitter();
 
 const hostname = '127.0.0.1';
 let port = 80;
 const inputArgs = process.argv;
-const putQueue = {};
-const getQueue = {};
 
+inputArgs.forEach((val, index) => {
+    if (val == "-p" && typeof(inputArgs[index+1]) != "undefined") {
+        port = inputArgs[index+1];
+    }
+  });
+
+
+class PutQueue {
+    constructor() {
+        this._queue = {};
+    }
+    pathInQueue(path) {
+        if (path in this._queue) {
+            return true
+        } else {
+            return false
+        }
+    }
+    addRequestToQueue(path, request) {
+        if (!(path in this._queue)) {
+            console.log(`no ${path} in queue`);
+            this._queue[path] = [request];
+            console.log(this._queue);
+        } else {
+            this._queue[path].push(request);
+            console.log(this._queue);
+        }
+    }
+    getFirstFromQueue(path) {
+        const result = this._queue[path].shift();
+        console.log(`get ${result} from ${path} in queue`);
+        if (this._queue[path].length == 0) {
+            delete this._queue[path];
+        }
+        return result
+    }
+}
+
+class GetQueue extends PutQueue {
+    getFirstFromQueue(path) {
+        return this._queue[path][0]
+    }
+    remRequestFromQueue(path, request, reason) {
+        switch(reason) {
+            case "timeout":
+                const rem = this._queue[path].indexOf(request);
+                this._queue[path].splice(rem, 1);
+                console.log(`deleted ${request} from ${path} Queue`);
+                if (this._queue[path].length == 0) {
+                    delete this._queue[path];
+                }
+                break;
+            default:
+                this._queue[path].shift()
+                if (this._queue[path].length == 0) {
+                    delete this._queue[path];
+                }
+                break;
+        }
+    }
+}
+
+const getQueue = new GetQueue();
+const putQueue = new PutQueue();
 myEmitter.setMaxListeners(Infinity)
 
 myEmitter.on('GET', (req, res) => {
     const url = new URL(req.url, `http://${hostname}/${req.url}`);
-    const keyV = url.searchParams.get("timeout");
+    const keyTimeout = url.searchParams.get("timeout");
     const path = url.pathname;
     const uniqId = Date.now();
-    if (!(path in getQueue)) {
-        console.log(`no ${path} in getQueue`);
-        getQueue[path] = [uniqId];
-        console.log(getQueue);
-    } else {
-        getQueue[path].push(uniqId);
-        console.log(getQueue);
-    }
-    console.log(keyV * 1000);
-    let counter = 0;;
+    getQueue.addRequestToQueue(path, uniqId);
+    console.log(keyTimeout * 1000);
+    let counter = 0;
     const interval = setInterval( function() {
         // console.log('Bar', counter);
-        if (uniqId == getQueue[path][0]) {
-            if (path in putQueue) {
+        if (uniqId == getQueue.getFirstFromQueue(path)) {
+            if (putQueue.pathInQueue(path)) {
                 console.log(`found ${path} in putQueue`);
                 res.statusCode = 200;
                 res.setHeader('Content-Type', 'text/plain');
-                res.end(`${putQueue[path].shift()}`);
-                if (putQueue[path].length == 0) {
-                    delete putQueue[path];
-                }
-                getQueue[path].shift()
-                if (getQueue[path].length == 0) {
-                    delete getQueue[path];
-                }
+                res.end(`${putQueue.getFirstFromQueue(path)}`);
+                getQueue.remRequestFromQueue(path, uniqId);
                 clearInterval(interval);
             }
-            
-            // testArg--
-            
         }
         counter++;
-        if (counter >= keyV*10) {
-            const rem = getQueue[path].indexOf(uniqId)
-            getQueue[path].splice(rem, 1)
-            console.log(`deleted ${uniqId} from ${path}getQueue`);
-            if (getQueue[path].length == 0) {
-                delete getQueue[path];
-            }
-            console.log(getQueue);
+        if (counter >= keyTimeout * 10) {
+            getQueue.remRequestFromQueue(path, uniqId, "timeout");
+            // console.log(getQueue);
             res.statusCode = 404;
             res.end();
             clearInterval(interval);
@@ -84,26 +123,11 @@ myEmitter.on('PUT', (req, res) => {
             } else {
                 res.statusCode = 200;
                 // testArg++
-                if (!(path in putQueue)) {
-                    console.log(`no ${path} in putQueue`);
-                    putQueue[path] = [keyV];
-                    console.log(putQueue);
-                } else {
-                    putQueue[path].push(keyV);
-                    console.log(putQueue);
-                }
-                // let waitTill = new Date(new Date().getTime() + 3 * 1000);
-                // while(waitTill > new Date()){}
-                res.setHeader('Content-Type', 'text/plain');
-                res.end(`PUT ${url.searchParams.get("v")}`);
+                putQueue.addRequestToQueue(path, keyV)
+                res.end();
                 return
             }
 })
-inputArgs.forEach((val, index) => {
-    if (val == "-p" && typeof(inputArgs[index+1]) != "undefined") {
-        port = inputArgs[index+1];
-    }
-  });
 
 const server = http.createServer((req, res) => {
     // console.log(req.method); 
